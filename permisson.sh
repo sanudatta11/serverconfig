b45581764e5276247d8d570c952108df207f0b74
Soumyajit Dutta <sanudatta11@users.noreply.github.com>
2017-05-11 18:44:05 +0530

    Added the permissons
    Add these permissons only when all the files are pulled from the github repository and synced with ELB properly. This file will grant the php and apache server to give the file access and read and write capability to the server.

---
+++ permisson.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Granting Image Permissions"
+sudo chown -R www-data:www-data /var/www
+sudo chmod go-rwx /var/www
+sudo chmod go+x /var/www
+sudo chgrp -R www-data /var/www
+
+sudo chmod -R go-rwx /var/www
+sudo chmod -R g+rx /var/www
+sudo chmod -R g+rwx /var/www
+#Now run this
+sudo chmod 0777 /var/www/html
+sudo chmod 0777 /var/www/
+
+echo "Granting Server Permissions"
+sudo chown -R www-data /var/www/html/
+sudo chgrp -R www-data /var/www/html/
+sudo chmod -R g+w /var/www/html/
+
+echo "Giving Question Save Permissons"
+sudo chmod 0777 /var/www/html/addcode/questions/
+sudo chmod 0777 /var/www/html/globalcontest/questions/
+
